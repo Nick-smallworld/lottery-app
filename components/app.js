69b3fcfbc658ce33f89ab7ae85daa3cd059914e8
Takeshi Nick Osanai <info@anythinggoes.tokyo>
2023-05-02 07:56:58 +0900

    change with tailwind.css

--- components/app.js
+++ components/app.js
@@ -1,39 +1,57 @@
-import React from 'react'
+import React from "react";
 
 const MyComponent = () => {
-
-  const handleClick = function() {
-
-    const arr = document.getElementById('textarea').value.split('\n')
-    const filteredArr = arr.filter(item => item != '')
+  const handleClick = function () {
+    const arr = document.getElementById("textarea").value.split("\n");
+    const filteredArr = arr.filter((item) => item != "");
 
     // 生成した配列からランダムに結果を一つ選び、結果をdivに表示する
 
-    const randomIndex = Math.floor(Math.random() * filteredArr.length)
-    const result = filteredArr != false ? filteredArr[randomIndex] : '入力データがありません' 
-    document.getElementById('result').innerHTML = result
-  }
+    const randomIndex = Math.floor(Math.random() * filteredArr.length);
+    const result =
+      filteredArr != false
+        ? filteredArr[randomIndex]
+        : "入力データがありません";
+    document.getElementById("result").innerHTML = result;
+  };
 
-  const clear = function(){
-    document.getElementById('clear').addEventListener('click', () => {
-        textarea.value = ''
-        document.getElementById('result').innerHTML = ''
-        }
-    );
-  }
+  const clear = function () {
+    textarea.value = "";
+    document.getElementById("result").innerHTML = "";
+  };
 
   return (
     <div>
-        <p>くじ引きしたい選択肢を改行区切りで入力してください。</p>
-        <form>
-        <textarea id="textarea" name="textarea" rows="5" cols="40"></textarea>
-        <div>
-            <button type="button" id="submit" onClick={handleClick}>くじを引く</button>
-            <button type="button" id="clear" onClick={clear}>クリア</button>
+      <p>くじ引きしたい選択肢を改行区切りで入力してください。</p>
+      <form>
+        <textarea
+          id="textarea"
+          name="textarea"
+          rows="5"
+          cols="40"
+          class="w-full p-2 bg-white border border-gray-300 rounded"
+        ></textarea>
+        <div class="mt-4">
+          <button
+            type="submit"
+            id="submit"
+            onClick={handleClick}
+            class="bg-blue-500 text-white px-4 py-2 rounded mr-2"
+          >
+            くじを引く
+          </button>
+          <button
+            type="button"
+            id="clear"
+            onClick={clear}
+            class="bg-red-500 text-white px-4 py-2 rounded"
+          >
+            Clear
+          </button>
         </div>
-        </form>
+      </form>
     </div>
-  )
-}
+  );
+};
 
 export default MyComponent;
--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,29 @@
-import Head from 'next/head';
-import styles from '../styles/Home.module.css';
-import Link from 'next/link';
-import React from 'react';
-import MyComponent from '../components/app';
+import Head from "next/head";
+import styles from "../styles/Home.module.css";
+import Link from "next/link";
+import React from "react";
+import MyComponent from "../components/app";
 
 export default function Home() {
   return (
     <div>
-        <h1>くじ</h1>
-            <MyComponent />
-        <p>結果</p>
-        <div id="result" ></div>
+      <Head>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>Tailwind CSS Template</title>
+        <link
+          href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.16/dist/tailwind.min.css"
+          rel="stylesheet"
+        />
+      </Head>
+      <div class="container mx-auto px-4">
+        <h1 class="text-4xl font-bold mb-4 mt-8">くじ</h1>
+        <MyComponent />
+        <div
+          id="result"
+          class="mt-8 p-4 bg-white border border-gray-300 rounded"
+        ></div>
+      </div>
     </div>
-
-    
-  )
+  );
 }
